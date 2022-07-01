c25d1e881061fd09561abb8ff08d84ccd516774c
Emanuel Mendez <emamendez6@gmail.com>
2022-07-01 15:37:30 -0300

    context

--- src/components/Context/CartItem.js
+++ src/components/Context/CartItem.js
@@ -16,21 +16,16 @@ export const CartItem = ({title, price, image, count}) => {
                 <h5 className="unprice"> Precio por unidad</h5>
                 <h5 className="untotalprice"> Total a pagar</h5>
         </div>
-        {
-            carrito.map((product)=>(
-                <div className="order-item">
+        <div className="order-item">
                 
                     <img className="image" src={image} alt={title} />
                     <p className="title">{title}.</p>
                     <p className="unidades">{quantity}</p>
                     <p className="unprice"> ${price} </p>
                     <p className="untotalprice"> ${totalprice} </p>
-                    <button className="btn420" onClick={()=>{removerDelCarrito(product.id)}}>remove</button>
-                </div>
-            ))
-        }
-        <div>
-            
+                    <button className="btn420" onClick={()=>{removerDelCarrito(carrito.id)}}>remove</button>
+        </div>
+        <div>            
             <button className="btn420" onClick={vaciarCarrito}>Vaciar</button>
             <button className="btn420">Finalizar</button>
         </div>
---
+++ src/components/Context/CartItemList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+
+function CartItemList ({carrito}) {
+    return (
+        carrito.map(p =>
+            <CartItem
+            key={p.id}
+            {...p}
+            title={p.title}
+            image={p.image}
+            price={p.price}
+            stock={p.stock}
+            category={p.category}
+            {...p}
+            quantity={p.quantity}
+            />
+            )
+    )
+}
+export default CartItemList;
