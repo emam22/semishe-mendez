8cece1fc2dd7bc644ae749e63d13c82a08a88f4b
Emanuel Mendez <emamendez6@gmail.com>
2022-07-18 19:58:07 -0300

    firebase

--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from 'react';
+import React, { useContext, useState} from 'react';
 import './ItemDetail.css';
 import ItemCount from '../itemCount/ItemCount';
 import {Link} from 'react-router-dom';
