1fde73b3918d7e6c4b553decbda69de8dfb9eaba
Emanuel Mendez <emamendez6@gmail.com>
2022-07-25 06:40:40 -0300

    firebase

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,7 +5,6 @@ import { collection, Timestamp, getDocs, writeBatch, query, where, documentId, a
 import { db } from "../../firebase/firebaseConfig";
 import Swal from "sweetalert";
 import './Form.css';
-import rick12 from "./img87.jpg";
 import { Button } from "@mui/material";
 import TextField from '@mui/material/TextField';
 
@@ -13,7 +12,7 @@ import validator from "validator";
 
 export const Form = () => {
 
-    const { cartItems, totalPrice, clearCart, cartCheckout}  = useContext(CartContext);
+    const { cartItems, vaciarCarrito, totalPrice, clearCart, cartCheckout}  = useContext(CartContext);
 
     const [buyerName, setBuyerName] = useState("");
     const [buyerEmail, setBuyerEmail] = useState("");
@@ -85,26 +84,7 @@ export const Form = () => {
             }
         })
 
-        if(outOfStock.length===0){
-            addDoc(ordenesCollection, orden)
-            .then((res)=>{
-                batch.commit()
-                
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Su orden ha sido registrada',
-                    text: `Su numero de orden es ${res.id}`
-                })
-            }) 
-        }else{
-            Swal.fire({
-                icon: 'error',
-                title: 'No hay stock de los siguientes productos: ',
-                text: outOfStock.map(ped=> ped.title).join(', ')
-            })
-        }
     }
-
     return(
         <>
             {
@@ -112,21 +92,15 @@ export const Form = () => {
             ? <Navigate to="/"/>
             :
             <>
-            <h2>Checkout Information:</h2>
+            <h2 className="formItems">Checkout Informacion:</h2>
             <TextField className="formItems" error={buyerName !== "" && !validName} required variant="filled" label="Full Name" onChange={handleNameChange} value={buyerName} />
             <TextField className="formItems" error={buyerPhone !== "" && !validPhone} required variant="filled" label="Phone Number" onChange={handlePhoneChange} value={buyerPhone} />
             <TextField className="formItems" error={buyerEmail !== "" && !validEmail} required variant="filled" label="Email Address" onChange={handleEmailChange} value={buyerEmail} />
             <TextField className="formItems" error={buyerEmailConf !== "" && !validEmailConf} required variant="filled" label="Confirm Email Address" onChange={handleEmailConfChange} value={buyerEmailConf} />
-            <div className="cartFooter dropShadow cart">
-                <div className="cartText">
-                    <h3>Cart Total: ${totalPrice}</h3>
-                    <h3>Total Products: {cartItems}</h3>
-                </div>
                 <div className="cartButtons">
                     <Button onClick={handleSubmit} variant="contained" disabled={(!validName || !validEmail || !validEmailConf || !validPhone)} color="success">Complete Purchase</Button>
                     <Button onClick={clearCart} variant="contained" color="error">Clear Cart</Button>
                 </div>
-            </div>
         </>
             }   
          </>
